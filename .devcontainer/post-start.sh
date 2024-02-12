3e5d10dc7d611095143411b9fe4c6866137f53c5
Mike Brevoort <mike.brevoort@gitpod.io>
2024-02-12 22:42:13 +0000

    devcontainer fixes

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export RWFW_PATH="/workspace/redwood"
+export RWFW_PATH="/workspaces/redwood"
 export REDWOOD_DISABLE_TELEMETRY=1
-cd /workspace/rw-test-app
+cd /workspaces/rw-test-app
 yarn rw dev
